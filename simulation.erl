5d124209ba6f4ed0b320d0323da412c08315e407
oates999 <oates999@gmail.com>
2017-10-26 17:59:41 -0400

    asadafsfasf

--- simulation.erl
+++ simulation.erl
@@ -1,14 +1,39 @@
 -module(simulation).
-
 -compile(export_all).
 
 -import(parser, [read/1]).
 
+spawn_from_list([]) ->
+	[];
+spawn_from_list([H|T]) ->
+	NewNode = start_node(H),
+	[ NewNode | spawn_from_list(T)].
+
+link_nodes_from_list(Nodes, N=1) ->
+	lists:nth(N, Nodes) ! { self(), { "neighbors", lists:nth(length(Nodes), Nodes), lists:nth(N+1, Nodes) } },
+	link_nodes_from_list(Nodes, 5);
+link_nodes_from_list(Nodes, N=5) ->
+	lists:nth(N, Nodes) ! { self(), { "neighbors", lists:nth(N-1, Nodes), lists:nth(1, Nodes) } }.
+
 run(Filename) ->
 	Data = parser:read(Filename),
-	Name = simulation:node(lists:nth(2, Data)),
-	Name.
+	Nodes = spawn_from_list(Data),
+	link_nodes_from_list(Nodes, 1),
+	Nodes.
+
+start_node(Dataset) ->
+	spawn(simulation, node, [Dataset]).
+
+node_looper() ->
+	receive
+		{From, { "neighbors", LeftNode, RightNode } } ->
+			L_NODE = LeftNode,
+			R_NODE = RightNode,
+			io:format("node:~p~n  left:~p~n  right:~p~n", [self(), L_NODE, R_NODE])
+	end,
+	node_looper().
 
 node(Dataset) ->
 	{ ID, HOST, NAME, PRIORITY, TOLERANCE } = Dataset,
-	NAME.
+	io:format("node created!~n"),
+	node_looper().
