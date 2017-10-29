f14c52798c7d238c08c50d3bd39be12ce35ecdd1
oates999 <oates999@gmail.com>
2017-10-29 18:26:14 -0400

    10-29-2017 work

--- simulation.erl
+++ simulation.erl
@@ -21,17 +21,32 @@ link_nodes_from_list(Nodes, N) ->
 	end,
 	case N of
 		NodesLength ->
-			io:format("neighbor linking complete.~n");
+			io:format("");
 		_ ->
 			link_nodes_from_list(Nodes, N+1)
+	end.
+
+kill_nodes(Nodes, N) ->
+	NodesLength = length(Nodes),
+	exit(lists:nth(N, Nodes), normal),
+	case N of
+		NodesLength ->
+			io:format("");
+		_ ->
+			kill_nodes(Nodes, N+1)
 	end.
 
 run(Filename) ->
 	Data = parser:read(Filename),
 	Nodes = spawn_from_list(Data),
 	link_nodes_from_list(Nodes, 1),
-	lists:nth(1, Nodes) ! { self(), { "time_update", 0, 0, 0, 0 } },
-	Nodes.
+	lists:nth(1, Nodes) ! { self(), { "faux_election" } },
+	receive
+		{ _, { "end_of_simulation" } } ->
+			kill_nodes(Nodes, 1),
+			io:format("End of simulation~n")
+	end,
+	exit(normal).
 
 start_node(Dataset) ->
 	spawn(simulation, node, [Dataset]).
@@ -44,7 +59,6 @@ node_looper(Dataset) ->
 			L_NODE = LeftNode,
 			R_NODE = RightNode,
 			NUM_NODES = NumNodes,
-			io:format("node:~p~n  left:~p~n  right:~p~n", [self(), L_NODE, R_NODE]),
 			node_looper({ ID, HOST, NAME, PRIORITY, TOLERANCE, SUPERVISOR, L_NODE, R_NODE, HAS_LEAD, NUM_NODES });
 		{ From, { "time_update", Time, LastElection, CurrentLeader, RevoltCounter } } ->
 			if
@@ -52,24 +66,45 @@ node_looper(Dataset) ->
 					if
 						RevoltCounter >= ((N+1) div 2) ->
 							io:format("ID=~p was deposed at t=~p~n", [ID, Time]),
-							SUP ! { self(), { "election" } },
+							L_N ! { self(), { "revolution", -1, self() } },
 							receive
-								{ From, { "results", NextLeader } } ->
-									NextLeader ! { self() , { "elected", Time+1, Time } }
+								{ From, { "revolution", HighestPriority, NextLeader } } ->
+									if
+										(HighestPriority =:= -1) ->
+											SUP ! { self(), { "end_of_simulation" } };
+										true ->
+											NextLeader ! { self(), { "elected", Time+1, Time } }
+									end
 							end;
 						true ->
 							L_N ! { self(), { "time_update", Time+1, LastElection, CurrentLeader, RevoltCounter } }
 					end;
-				(ID =/= CurrentLeader) and (Time-LastElection >= TOLERANCE) and (Time-LastElection < TOLERANCE + N) ->
-					io:format("ID=~p has revolted at t=~p... tolerance=~p~n", [ID, Time, TOLERANCE]),
+				(ID =/= CurrentLeader) and (Time-LastElection > TOLERANCE) and (Time-LastElection =< TOLERANCE + N) ->
+					io:format("ID=~p revolted at t=~p~n", [ID, Time]),
 					L_N ! { self(), { "time_update", Time+1, LastElection, CurrentLeader, RevoltCounter+1 } };
 				true ->
 					L_N ! { self(), { "time_update", Time+1, LastElection, CurrentLeader, RevoltCounter } }
+			end;
+		{ _, { "elected", Time, LastElection } } ->
+			io:format("ID=~p became leader at t=~p~n", [ID, Time]),
+			L_N ! { self(), { "time_update", Time+1, LastElection, ID, 0 } },
+			node_looper({ ID, HOST, NAME, PRIORITY, TOLERANCE, SUP, L_N, R_N, true, N });
+		{ _, { "revolution", HighestPriority, NextLeader } } ->
+			if
+				((HAS_LEAD =:= false) and (HighestPriority < PRIORITY)) ->
+					L_N ! { self(), { "revolution", PRIORITY, self() } };
+				true ->
+					L_N ! { self(), { "revolution", HighestPriority, NextLeader } }
+			end;
+		{ _, { "faux_election" } } ->
+			L_N ! { self(), { "revolution", PRIORITY, self() } },
+			receive
+				{ _, { "revolution", _, NextLeader } } ->
+					NextLeader ! { self(), { "elected", 0, 0 } }
 			end
 	end,
 	node_looper({ ID, HOST, NAME, PRIORITY, TOLERANCE, SUP, L_N, R_N, HAS_LEAD, N }).
 
 node(Dataset) ->
 	{ ID, HOST, NAME, PRIORITY, TOLERANCE } = Dataset,
-	io:format("node created!~n"),
 	node_looper({ ID, HOST, NAME, PRIORITY, TOLERANCE, 0, 0, 0, false, 0 }).
